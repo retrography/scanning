75f03bf170d235ef2c205c681d760a4cbbb10882
Mahmood Shafeie Zargar <mahmood@gmail.com>
2017-05-22 01:11:43 +0200

    scaneg with vips

---
+++ scaneg/negcut.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# coding: utf-8
+
+import sys
+import gi
+gi.require_version('Vips', '8.0')
+from gi.repository import Vips
+import math
+
+CPERCENT = 99.9
+CTHRESHOLD = 10
+BLACK = 2 * 256
+WHITE = 170 * 256
+
+
+
--- scaneg/negfix9.py
+++ scaneg/negfix9.py
@@ -3,6 +3,7 @@
 # coding: utf-8
 
 import sys
+import os
 import gi
 import math
 gi.require_version('Vips', '8.0')
@@ -10,11 +11,11 @@ from gi.repository import Vips
 
 #TODO: Test BW
 #TODO: Negfix profiler not implemeted yet
-#TODO: IR separation not implemented yet
 #TODO: Resize not implemented yet
 #TODO: Frame cut not implemented yet
 #TODO: Dust removal not implemented yet
 
+os.environ["VIPS_WARNING"] = "0"
 
 def profile(im):
     temp = im.extract_area(10, 10, im.width - 20, im.height - 20).gaussblur(3, min_ampl=0.5)
@@ -119,15 +120,80 @@ def cstretch(im):
 
     return im.cast(bandfmt)
 
-infile = sys.argv[1]
+
+# Detect film base color
+def filmbase(im):
+    return [int(band.percent(CPERCENT) - band.deviate() / 2) for band in im.bandsplit()]
+
+
+# def frame(im):
+#     return min([band.min() < BLACK for band in im.bandsplit()])
+
+
+# def space(im):
+#     return min([band.max() > WHITE for band in im.bandsplit()])
+
+
+# Detect trim coordinates
+def trimco(im):
+
+    imm = im.median(3)
+    background = filmbase(imm)
+
+    threshold = (int((pow(imm.deviate(), 2) * CTHRESHOLD) / (256 * 256)))
+    print(threshold)
+    mask = (imm - background).abs() > threshold
+
+    # sum mask rows and columns, then search for the first non-zero sum in each
+    # direction
+    columns, rows = mask.project()
+
+    # .profile() returns a pair (v-profile, h-profile)
+    left = columns.profile()[1].min()
+    right = columns.width - columns.flip("horizontal").profile()[1].min()
+    top = rows.profile()[0].min()
+    bottom = rows.height - rows.flip("vertical").profile()[0].min()
+
+    # and now crop the original image
+
+    return left, top, right - left, bottom - top
+
+
+######### THE SCRIPT #########
+
+if len(sys.argv) <= 1:
+    print("Need an image to work on!")
+elif len(sys.argv) >= 2:
+    infile = sys.argv[1]
+    if len(sys.argv) >= 3:
+        outfile = sys.argv[2]
+    else:
+        outfile = 'p_' + infile
+
 image = Vips.Image.new_from_file(infile)
+
+# Resize based on resolution
 
 resolution = int(image.xres * 25.4)
 
+# TODO: need a switch for resize
 if resolution == 4800:
-    image = image.shrink(3, 3)
+    image = image.shrink(3, 3).copy(xres=image.xres / 3.0, yres=image.yres / 3.0)
+    resolution /= 3
 elif resolution in [1200, 2400]:
-    image = image.shrink(2, 2)
+    image = image.shrink(2, 2).copy(xres=image.xres / 2.0, yres=image.yres / 2.0)
+    resolution /= 2
+
+if image.Bands == 4:
+    ir = image[3]
+    image = image[0].bandjoin(image[1:3])
+elif image.Bands == 2:
+    ir = image[1]
+    image = image[0]
+else:
+    ir = None
+
+# Detect the number of bands
 
 if image.bands >= 3:
     color = True
@@ -135,6 +201,14 @@ if image.bands >= 3:
 else:
     color = False
     nobands = 1
+
+# Get trim coordinates
+CPERCENT = 99.9
+CTHRESHOLD = 10
+BLACK = 2 * 256
+WHITE = 170 * 256
+
+coords = trimco(image)
 
 bandfmt = image.BandFmt
 gamma = 2.15
@@ -144,11 +218,20 @@ prof = profile(image)
 
 image = invert(image)
 
-#TODO: create a commandline option for shrecovery
-#image = shrecovery(image)
+# Trim
+image = image.crop(coords[0], coords[1], coords[2], coords[3])
+if ir:
+    ir = ir.crop(coords[0], coords[1], coords[2], coords[3])
 
-#TODO: create a commandline option for cstretch
-#image = cstretch(image)
+if nobands > 1:
+    #TODO: create a commandline option for shrecovery
+    image = shrecovery(image)
 
-image.write_to_file("p_"+infile)
+    #TODO: create a commandline option for cstretch
+    image = cstretch(image)
+
+if ir:
+    image = image.bandjoin(ir)
+
+image.write_to_file(outfile)
 
